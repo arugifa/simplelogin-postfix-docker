b63b2b6a8e0a438a457e349e8ecb6c879945f907
Alexandre Figura <arugifa@users.noreply.github.com>
2021-04-14 14:27:35 +0200

    [FIX] Use certbot and not cerbot to renew SSL certificate (#5)

--- scripts/certbot-renew-crontab.sh
+++ scripts/certbot-renew-crontab.sh
@@ -5,7 +5,7 @@ CERTIFICATE="/etc/letsencrypt/live/$POSTFIX_FQDN/fullchain.pem"
 PRIVATE_KEY="/etc/letsencrypt/live/$POSTFIX_FQDN/privkey.pem"
 
 if [ -f $CERTIFICATE -a -f $PRIVATE_KEY ]; then
-    cerbot -n renew
+    certbot -n renew
 else
     certbot -n certonly
 fi
